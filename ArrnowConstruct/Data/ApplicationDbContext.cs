abddd3b0d3ef68c658d591d31cb46378065c90bd
kresa <kresayuts@gmail.com>
2022-11-10 00:45:04 +0200

    Created entities. Made validations with attributes. Haven't exsecudetd them in the database. Still designing the structure.

--- ArrnowConstruct/Data/ApplicationDbContext.cs
+++ ArrnowConstruct/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ArrnowConstruct.Infrastructure.Data
+namespace ArrnowConstruct.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
